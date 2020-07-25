9dc68e993330766b1296f88d81c7d6d7f2ad2c66
yogeshsingh <yogeshsingh101200@gmail.com>
2020-07-25 23:27:42 +0530

    style tweaks

--- helpers.py
+++ helpers.py
@@ -1,14 +1,16 @@
+""" Helper functions for application.py """
+
 import os
-import requests
+from functools import wraps
 import urllib.parse
 
-from flask import redirect, render_template, request, session
-from functools import wraps
+import requests
+from flask import redirect, render_template, session
 
 
 def apology(message, code=400):
     """Render message as an apology to user."""
-    def escape(s):
+    def escape(string):
         """
         Escape special characters.
 
@@ -16,11 +18,11 @@ def apology(message, code=400):
         """
         for old, new in [("-", "--"), (" ", "-"), ("_", "__"), ("?", "~q"),
                          ("%", "~p"), ("#", "~h"), ("/", "~s"), ("\"", "''")]:
-            s = s.replace(old, new)
-        return s
+            string = string.replace(old, new)
+        return string
     return render_template("apology.html", top=code, bottom=escape(message)), code
 
-    
+
 def login_required(f):
     """
     Decorate routes to require login.
@@ -42,7 +44,8 @@ def lookup(symbol):
     try:
         api_key = os.environ.get("API_KEY")
         response = requests.get(
-            f"https://cloud-sse.iexapis.com/stable/stock/{urllib.parse.quote_plus(symbol)}/quote?token={api_key}")
+            f"""https://cloud-sse.iexapis.com/stable/stock/
+            {urllib.parse.quote_plus(symbol)}/quote?token={api_key}""")
         response.raise_for_status()
     except requests.RequestException:
         return None
