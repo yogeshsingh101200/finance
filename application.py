007799b7801faabc751de0fc4fea243e6e49ee4c
yogeshsingh <yogeshsingh101200@gmail.com>
2020-07-25 13:58:10 +0530

    removed dependency on cs50 library

--- application.py
+++ application.py
@@ -1,9 +1,11 @@
+""" Logic for webserver of finance application """
 import os
 
 import re
 
-from cs50 import SQL
 from flask import Flask, flash, jsonify, redirect, render_template, request, session
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
 from flask_session import Session
 from tempfile import mkdtemp
 from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
@@ -37,8 +39,13 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-# Configure CS50 Library to use SQLite database
-db = SQL("sqlite:///finance.db")
+# Make sure DATABASE url is set
+if not os.getenv("DATABASE_URL"):
+    raise RuntimeError("DATABASE_URL is not set")
+
+# Configuration to use database
+engine = create_engine(os.getenv("DATABASE_URL"), echo=True)
+db = scoped_session(sessionmaker(bind=engine))
 
 # Make sure API key is set
 if not os.environ.get("API_KEY"):
@@ -55,7 +62,9 @@ def index():
 
     # Queries distinct list of symbols and there total shares
     rows = db.execute(
-        "SELECT DISTINCT symbol, SUM(shares) FROM history WHERE id = :id GROUP BY symbol", id=session["user_id"])
+        "SELECT DISTINCT symbol, SUM(shares) FROM history WHERE id = :id GROUP BY symbol", {
+            "id": session["user_id"]
+        })
 
     # To store total valuation of shares
     totPrice = 0
@@ -76,13 +85,14 @@ def index():
             totPrice += row["SUM(shares)"] * quote["price"]
 
     # For showing available cash
-    cash = db.execute("SELECT cash FROM users WHERE id = :id",
-                      id=session["user_id"])
+    cash = db.execute("SELECT cash FROM users WHERE id = :id", {
+        "id": session["user_id"]
+    }).fetchone()
 
     # For showing grand total
-    grandTotal = totPrice + cash[0]["cash"]
+    grandTotal = totPrice + cash["cash"]
 
-    return render_template("index.html", table=table, cash=cash[0]["cash"], grandTotal=grandTotal)
+    return render_template("index.html", table=table, cash=cash["cash"], grandTotal=grandTotal)
 
 
 @app.route("/buy", methods=["GET", "POST"])
@@ -113,18 +123,27 @@ def buy():
         # Checks for affordability
         price = int(shares) * quote["price"]
         cash = db.execute(
-            "SELECT cash FROM users WHERE id = :id", id=session["user_id"])
-        if cash[0]["cash"] < price:
+            "SELECT cash FROM users WHERE id = :id", {"id": session["user_id"]}).fetchone()
+        if cash["cash"] < price:
             return apology("CAN'T AFFORD!")
 
         # Records
-        db.execute("INSERT INTO history (id, symbol, shares, price) VALUES (:id, :symbol, :shares, :price)",
-                   id=session["user_id"], symbol=quote["symbol"], shares=int(shares), price=price)
+        db.execute("""
+        INSERT INTO history(id, symbol, shares, price)
+        VALUES(: id, : symbol, : shares, : price)""", {
+            "id": session["user_id"],
+            "symbol": quote["symbol"],
+            "shares": int(shares),
+            "price": price
+        })
 
         # Update
-        cash = cash[0]["cash"] - price
-        db.execute("UPDATE users SET cash = :cash WHERE id = :id",
-                   cash=cash, id=session["user_id"])
+        cash = cash["cash"] - price
+        db.execute("UPDATE users SET cash = :cash WHERE id = :id", {
+            "cash": cash, "id": session["user_id"]
+        })
+
+        db.commit()
 
         # Notifies to user
         flash("BOUGHT!")
@@ -142,7 +161,10 @@ def check():
     username = request.args.get("username")
 
     # Queries and check database for availability of username
-    if db.execute("SELECT username FROM users WHERE username = :username", username=username):
+    row = db.execute("SELECT username FROM users WHERE username = :username",
+                     {"username": username}).fetchone()
+
+    if row:
         return jsonify(False)
     else:
         return jsonify(True)
@@ -154,8 +176,9 @@ def history():
     """Show history of transactions"""
 
     # Query history table to get all the data to display at history.html
-    rows = db.execute("SELECT * FROM history WHERE id = :id",
-                      id=session["user_id"])
+    rows = db.execute("SELECT * FROM history WHERE id = :id", {
+        "id": session["user_id"]
+    })
     return render_template("history.html", rows=rows)
 
 
@@ -178,16 +201,17 @@ def login():
             return apology("MUST PROVIDE PASSWORD!", 403)
 
         # Query database for username
-        rows = db.execute("SELECT * FROM users WHERE username = :username",
-                          username=request.form.get("username"))
+        row = db.execute("SELECT * FROM users WHERE username = :username", {
+            "username": request.form.get("username")
+        }).fetchone()
 
         # Ensure username exists and password is correct
-        if len(rows) != 1 or not check_password_hash(rows[0]["hash"],
-                                                     request.form.get("password")):
+        if not row or not check_password_hash(row["hash"],
+                                              request.form.get("password")):
             return apology("INVALID USERNAME/PASSWORD!", 403)
 
         # Remember which user has logged in
-        session["user_id"] = rows[0]["id"]
+        session["user_id"] = row["id"]
 
         # Redirect user to home page
         return redirect("/")
@@ -245,7 +269,7 @@ def register():
         username = request.form.get("username")
         if not username:
             return apology("MUST PROVIDE USERNAME!")
-        elif db.execute("SELECT username FROM users WHERE username = :username", username=username):
+        elif db.execute("SELECT username FROM users WHERE username = :username", {"username": username}).fetchone():
             return apology("USERNAME NOT AVAILABLE!")
         password = request.form.get("password")
         if not password:
@@ -262,8 +286,11 @@ def register():
         hash = generate_password_hash(password)
 
         # Inserts user into database(name=users)
-        db.execute("INSERT INTO users(username, hash) VALUES(:username, :hash)",
-                   username=username, hash=hash)
+        db.execute("INSERT INTO users(username, hash) VALUES(:username, :hash)", {
+            "username": username,
+            "hash": hash
+        })
+        db.commit()
 
         # Redirects to homepage
         return redirect("/")
@@ -285,9 +312,10 @@ def sell():
         # Validation
         if not symbol:
             return apology("NO SYMBOL SELECTED!")
-        rows = db.execute("SELECT SUM(shares) FROM history WHERE id = :id AND symbol = :symbol",
-                          id=session["user_id"], symbol=symbol)
-        if rows[0]["SUM(shares)"] is None:
+        row = db.execute("SELECT SUM(shares) FROM history WHERE id = :id AND symbol = :symbol", {
+            "id": session["user_id"], "symbol": symbol
+        }).fetchone()
+        if row["SUM(shares)"] is None:
             return apology("YOU DON'T HAVE SHARES OF THIS COMPANY!")
         shares = request.form.get("shares")
         if not shares:
@@ -296,20 +324,27 @@ def sell():
             return apology("INVALID SHARES PROVIDED!")
 
         # Checks affordability
-        if int(shares) > rows[0]["SUM(shares)"]:
-            return apology("YOU HAVE ONLY {} SHARES OF THIS COMPANY!".format(rows[0]["SUM(shares)"]))
+        if int(shares) > row["SUM(shares)"]:
+            return apology("YOU HAVE ONLY {} SHARES OF THIS COMPANY!".format(row["SUM(shares)"]))
 
         # Records in history table
         quote = lookup(symbol)
-        db.execute("INSERT INTO history (id, symbol, shares, price) VALUES (:id, :symbol, :shares, :price)",
-                   id=session["user_id"], symbol=quote["symbol"], shares=-int(shares), price=quote["price"])
+        db.execute("INSERT INTO history (id, symbol, shares, price) VALUES (:id, :symbol, :shares, :price)", {
+            "id": session["user_id"],
+            "symbol": quote["symbol"],
+            "shares": -int(shares),
+            "price": quote["price"]
+        })
 
         # Updates cash in users table
         cash = db.execute(
-            "SELECT cash FROM users WHERE id = :id", id=session["user_id"])
-        updatedCash = cash[0]["cash"] + quote["price"] * int(shares)
-        db.execute("UPDATE users SET cash = :cash WHERE id = :id",
-                   cash=updatedCash, id=session["user_id"])
+            "SELECT cash FROM users WHERE id = :id", {"id": session["user_id"]}).fetchone()
+        updatedCash = cash["cash"] + quote["price"] * int(shares)
+        db.execute("UPDATE users SET cash = :cash WHERE id = :id", {
+            "cash": updatedCash, "id": session["user_id"]
+        })
+
+        db.commit()
 
         # Notifies to user
         flash("SOLD!")
@@ -320,7 +355,9 @@ def sell():
     # If users reches to route via get like by clicking on sell tab
     else:
         rows = db.execute(
-            "SELECT DISTINCT symbol, SUM(shares) FROM history WHERE id = :id GROUP BY symbol", id=session["user_id"])
+            "SELECT DISTINCT symbol, SUM(shares) FROM history WHERE id = :id GROUP BY symbol", {
+                "id": session["user_id"]
+            })
         return render_template("sell.html", rows=rows)
 
 
@@ -334,9 +371,9 @@ def change():
         oldPassword = request.form.get("oldPassword")
         if not oldPassword:
             return apology("PROVIDE OLD PASSWORD TO CONTINUE!")
-        rows = db.execute(
-            "SELECT hash FROM users WHERE id = :id", id=session["user_id"])
-        if not check_password_hash(rows[0]["hash"], oldPassword):
+        row = db.execute(
+            "SELECT hash FROM users WHERE id = :id", {"id": session["user_id"]}).fetchone()
+        if not check_password_hash(row["hash"], oldPassword):
             return apology("INAVLID PASSWORD!")
 
         # New password validation
@@ -345,13 +382,19 @@ def change():
             return apology("PROVIDE NEW PASSWORD!")
         if len(newPassword) < 8:
             return apology("THE PASSWORD MUST CONTAIN ATLEAST 8 CHARACTERS!")
-        if not (re.search(r"[0-9]", newPassword) and re.search(r"[a-z]", newPassword) and re.search(r"[A-Z]", newPassword) and re.search(r"\W", newPassword)):
+        if not (re.search(r"[0-9]", newPassword)
+                and re.search(r"[a-z]", newPassword)
+                and re.search(r"[A-Z]", newPassword)
+                and re.search(r"\W", newPassword)):
             return apology("THE PASSWORD MUST CONTAIN ATLEAST ONE OUT OF 0-9, a-z, A-Z AND SPECIAL CHARACTERS!")
 
         # Updating the database
         hash = generate_password_hash(newPassword)
-        db.execute("UPDATE users SET hash = :hash WHERE id = :id",
-                   hash=hash, id=session["user_id"])
+        db.execute("UPDATE users SET hash = :hash WHERE id = :id", {
+            "hash": hash,
+            "id": session["user_id"]
+        })
+        db.commit()
 
         # Inform the user
         flash("PASSWORD CHANGED SUCCESSFULLY!")
