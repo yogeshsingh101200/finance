f1943924121025ba84f150bc198ce262e4420048
yogeshsingh <yogeshsingh101200@gmail.com>
2020-07-26 23:58:37 +0530

    now using default flask directory for user sessions

--- application.py
+++ application.py
@@ -4,7 +4,6 @@ import os
 import re
 
 from datetime import datetime
-from tempfile import mkdtemp
 from flask import Flask, flash, jsonify, redirect, render_template, request, session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
@@ -34,7 +33,6 @@ def after_request(response):
 app.jinja_env.filters["usd"] = usd
 
 # Configure session to use filesystem (instead of signed cookies)
-app.config["SESSION_FILE_DIR"] = mkdtemp()
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
